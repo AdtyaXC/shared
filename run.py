a54e80871213845edb2b9c8e8db2ccbb9132c47d
Aditya Putra Mahesa XD <113027344+AdtyaXC@users.noreply.github.com>
2022-11-08 05:23:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 if __name__ == '__main__':
     try:
         os.system("git pull")
-        __import__("run").AdtyaStart()
+        __import__("run")
     except Exception as e:
         exit(str(e))
